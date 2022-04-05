e1aa4528bc9ec6fa12310af240d29ef5fa725b11
Paul <oluwadamilare321@gmail.com>
2022-04-05 22:32:29 +0100

    navbar init

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,11 +1,21 @@
 <template>
-    <div></div>
+    <div class="sideNav">
+      <!-- <div class="logo">
+        <h2 class="text-white italic"><span class="text-[#f21137] font-bold font-mono">X</span>LAM</h2>
+      </div> -->
+      <router-link>Home</router-link>
+      <router-link>Services</router-link>
+      <router-link>Contact Us</router-link>
+      <router-link>FAQs</router-link>
+      <router-link>Testimonials</router-link>
+    </div>
 </template>
 
 // <script lang="ts">
 // import { defineComponent } from 'vue'
 
 // export default defineComponent({
+  // name: 'NavBar',
 //   setup() {
 //     const
 //   },
@@ -13,5 +23,14 @@
 // </script>
 
 <style scoped>
-
+.sideNav {
+  width: 250px;
+  height: 100vh;
+  position: fixed;
+  right: -250px;
+  top: 0;
+  background: #f21137;
+  z-index: 2;
+  transition: 0.5s;
+}
 </style>
--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,10 @@ import router from './router'
 import store from './store'
 import './index.css'
 
+// global registration of components
+import NavBar from './components/NavBar.vue'
+import TheFooter from './components/TheFooter.vue'
+
+Vue.component('NavBar', NavBar)
+Vue.component('TheFooter', TheFooter)
 createApp(App).use(store).use(router).mount('#app')
