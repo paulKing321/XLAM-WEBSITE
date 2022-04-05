ba55afde5db3917b6fde24d2d7c648448ed167ca
Paul <oluwadamilare321@gmail.com>
2022-04-05 15:12:13 +0100

    home page first viewport

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,25 @@
 <template>
-  <nav>
-    <!-- <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> -->
-  </nav>
+  <!-- <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav> -->
   <router-view/>
 </template>
 
 <style>
+*{
+    margin: 0;
+    padding: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  /* color: #2c3e50; */
 }
 
-nav {
+/* nav {
   padding: 30px;
 }
 
@@ -26,5 +30,5 @@ nav a {
 
 nav a.router-link-exact-active {
   color: #42b983;
-}
+} */
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,9 +2,11 @@
   <div class="home">
     <!-- first viewport/ -->
     <div class="first">
-      <h1>XLAM Multimedia</h1>
-      <p>Your surest audio visual equipments plug</p>
-      <div class="banner-btn">
+      <div class="first-text">
+        <h1><span class="red-text">X</span>LAM <span class="red-text">M</span>ultimedia</h1>
+        <p>Your surest audio visual equipments plug</p>
+      </div>
+      <div class="first-btn">
         <a href="#"><span></span>Find Out</a>
         <a href="#"><span></span>Read More</a>
       </div>
@@ -30,5 +32,51 @@ export default class HomeView extends Vue {}
   background-position: center;
   height: 100vh;
 }
-
+.first-text {
+  text-align: center;
+  color: #fff;
+  padding-top: 180px;
+}
+.first-text h1{
+  font-size: 130px;
+  font-family: 'Kaushan Script', cursive;
+}
+.first-text p{
+  font-size: 20px;
+  font-style: italic;
+}
+.red-text {
+  color: #F21137;
+}
+.first-btn{
+  margin: 70px auto 0;
+}
+.first-btn a{
+  width: 150px;
+  text-decoration: none;
+  display: inline-block;
+  margin: 0 10px;
+  padding: 12px 0;
+  color: #fff;
+  border: .5px solid #fff;
+  position: relative;
+  z-index: 1;
+  transition: color 0.5s;
+}
+.first-btn a span{
+  width: 0;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background: #fff;
+  z-index: -1;
+  transition: 0.5s;
+}
+.first-btn a:hover span{
+  width: 100%;
+}
+.first-btn a:hover{
+  color: #000;
+}
 </style>
