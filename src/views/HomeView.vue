a8535fdbb2a5f8ab74fce2f3f3a5f8e0c9af2b94
Paul <oluwadamilare321@gmail.com>
2022-04-05 14:42:48 +0100

    basic structure init

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <!-- first viewport/ -->
+    <div class="first">
+      <h1>XLAM Multimedia</h1>
+      <p>Your surest audio visual equipments plug</p>
+      <div class="banner-btn">
+        <a href="#"><span></span>Find Out</a>
+        <a href="#"><span></span>Read More</a>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+// import  from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 @Options({
   components: {
-    HelloWorld,
-  },
+  }
 })
 export default class HomeView extends Vue {}
 </script>
+
+<style scoped>
+.first {
+  background: linear-gradient(rgba(0,0,0,0.5),#68020F), url(../assets/images/picture-8.jpg);
+  background-size: cover;
+  background-position: center;
+  height: 100vh;
+}
+
+</style>
