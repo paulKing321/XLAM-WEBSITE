d7ee58c21d7f3a520e6a9919dd5030ffbc3a0f77
Paul <oluwadamilare321@gmail.com>
2022-04-05 22:10:09 +0100

    second viewport completed

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@
     padding: 0;
 }
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Kaushan Script', cursive;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/components/home/SecondViewport.vue
+++ src/components/home/SecondViewport.vue
@@ -1,6 +1,11 @@
 <template>
-  <div class="second">
-   1 
+  <div class="bg-[#FFFFFE] w-4/5 mx-auto min-h-screen mt-8 md:mt-12 second py-8 md:py-12">
+   <h1 class="text-2xl md:text-4xl lg:text-5xl xl:text-6xl font-bold text-[#f21137]">WELCOME TO XLAM Multimedia.</h1>
+   <p class="text-lg md:text-xl italic mt-6 leading-8">We Rent / Sell Audio and Visual Equipments for Companies, Schools and Individual use - Projector, Projector Screens, Flip Chart with Mobile Stand, Plasma TV Screens, Rechargeable Public Address Systems with Wireless Microphones etc. We rent projector in Ibadan and its environs - we deliver nationwide.</p>
+   <div class="pictures flex flex-col md:flex-row gap-y-8 md:gap-y-o md:gap-x-8 w-5/5 md:w-4/5 my-12 mx-auto items-center justify-center">
+     <img class="rounded-lg border-x-[#f21137] border-y-[#030303] border-4 w-11/12 md:w-3/5 " :src="require('@/assets/images/picture-1.jpg')" alt="">
+     <img class="rounded-xl border-x-[#f21137] border-y-[#030303] border-4 w-11/12 md:w-3/5 " :src="require('@/assets/images/picture-projector.jpg')" alt="">
+   </div>
   </div>
 </template>
 
@@ -8,9 +13,8 @@
 // import { defineComponent } from 'vue'
 
 // export default defineComponent({
-//     setup() {
-        
-//     },
+//   setup() {
+//   },
 // })
 // </script>
 
