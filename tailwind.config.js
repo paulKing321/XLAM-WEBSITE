a1967084fa61810f3448d2b59bcede17b8ca750c
Paul <oluwadamilare321@gmail.com>
2022-04-05 16:08:01 +0100

    tailwind added

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/components/NavBar.vue",
+    "./src/components/TheFooter.vue",
+    "./src/components/home/FirstViewport.vue",
+    "./src/components/home/SecondViewport.vue",
+    "./src/views/HomeView.vue",
+    "./src/views/AboutView.vue"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
