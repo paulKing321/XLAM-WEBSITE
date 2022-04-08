85cfbf9c70e1886238533cbde4d7bc16aa8e08e2
Paul <oluwadamilare321@gmail.com>
2022-04-08 14:51:06 +0100

    Contact us page

--- src/components/home/SecondViewport.vue
+++ src/components/home/SecondViewport.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="bg-[#FFFFFE] w-4/5 mx-auto min-h-screen mt-8 md:mt-12 second py-8 md:py-12">
    <h1 class="text-2xl md:text-4xl lg:text-5xl xl:text-6xl font-bold text-[#f21137]">WELCOME TO XLAM Multimedia.</h1>
-   <p class="text-lg md:text-xl italic mt-6 leading-8">We Rent / Sell Audio and Visual Equipments for Companies, Schools and Individual use - Projector, Projector Screens, Flip Chart with Mobile Stand, Plasma TV Screens, Rechargeable Public Address Systems with Wireless Microphones etc. We rent projector in Ibadan and its environs - we deliver nationwide.</p>
-   <div class="pictures flex flex-col md:flex-row gap-y-8 md:gap-y-o md:gap-x-8 w-5/5 md:w-4/5 my-12 mx-auto items-center justify-center">
-     <img class="rounded-lg border-x-[#f21137] border-y-[#030303] border-4 w-11/12 md:w-3/5 " :src="require('@/assets/images/picture-1.jpg')" alt="">
-     <img class="rounded-xl border-x-[#f21137] border-y-[#030303] border-4 w-11/12 md:w-3/5 " :src="require('@/assets/images/picture-projector.jpg')" alt="">
+   <p class="mt-6 text-lg italic leading-8 md:text-xl">We Rent / Sell Audio and Visual Equipments for Companies, Schools and Individual use - Projector, Projector Screens, Flip Chart with Mobile Stand, Plasma TV Screens, Rechargeable Public Address Systems with Wireless Microphones etc. We rent projector in Ibadan and its environs - we deliver nationwide.</p>
+   <div class="flex flex-col items-center justify-center mx-auto my-12 pictures md:flex-row gap-y-8 md:gap-y-o md:gap-x-8 w-5/5 md:w-4/5">
+     <img class="w-11/12 rounded-lg md:w-3/5 " :src="require('@/assets/images/picture-1.jpg')" alt="">
+     <img class="w-11/12 rounded-xl md:w-3/5 " :src="require('@/assets/images/picture-projector.jpg')" alt="">
    </div>
   </div>
 </template>
---
+++ src/views/ContactUs.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="contact">
+    <h1>Contact</h1>
+    <ul>
+      <li v-for="icon in icons" :key="icon.name">
+        <span class="material-icons">{{ icon.name }}</span>
+        <div>{{ icon.text }}</div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const icons = ref ([
+      {name: 'alternate_email', text: 'by email'},
+      {name: 'alternate_email', text: 'by email'},
+      {name: 'alternate_email', text: 'by email'},
+      {name: 'alternate_email', text: 'by email'}
+    ])
+
+    return { icons }
+  }
+}
+</script>
+
+<style scoped>
+.contact ul {
+  padding: 0;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 20px;
+  max-width: 400px;
+  margin: 60px auto;
+}
+.contact li {
+  list-style-type: none;
+  background: white;
+  padding: 30px;
+  border-radius: 10px;
+  box-shadow: 1px 3px 5px #030303;
+  cursor: pointer;
+  line-height: 1.5rem;
+}
+</style>
